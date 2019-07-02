ea599f38101c6da658f78e08b739fb6e3e278b5d
jfmolano <jose.francisco.molano@gmail.com>
2019-07-01 22:06:59 -0500

    class 2

--- clase1.js
+++ clase1.js
@@ -1,3 +1,12 @@
 console.log('Hello world JS')
 var name = 'José', lastname = 'Molano'
+var nameUpper = name.toUpperCase()
+var lastnameLower = name.toLowerCase()
+var nameFirstLetter = name.charAt(0)
+var nameSize = name.length
+var fullName = name + ' ' + lastname
+var sliceStr = name.substr(1,2)
+
+//Text interpolation
+var fullnNameInterpolated = `${name} ${lastname.toUpperCase()}`
 console.log('Hello ' + name + ' ' + lastname)
